e8f92e075c367762bfe3e7ab06b9906a29158da0
Werner Griesel <wernerg@dotslash.co.za>
2017-12-07 16:57:29 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using lexus.Models;
+
+namespace lexus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{*page}")]
+        public IActionResult Route(string page)
+        {
+            if (page == null)
+                page = "Index";
+
+            return View("~/Views/Home/" + page + ".cshtml");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Navigation ACTIVE class
+$(document).ready(function(){
+	$('.navbar-nav li a[href="' + document.location.pathname + '"]').parent('li').addClass('active');
+});
+
+// Header SWITCH
+$(document).ready(function(){
+    var navbar = $("#main-nav");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 300) {
+            navbar.removeClass('nav-padding');
+        } else {
+            navbar.addClass('nav-padding');
+        }
+    });
+});
